b002a8ff34a63c76efa5a316bd68501462fab00f
Robert Jan Hogerbrugge <rj.hogerbrugge@hotmail.com>
2017-03-19 16:01:28 +0100

    minor flexbox and photo styling

--- src/cats/CatsContainer.js
+++ src/cats/CatsContainer.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux'
 import { App } from '../App'
 import CatsEditor from '../cats/CatsEditor'
 import fetchCats from '../actions/cats/fetch'
+import './CatsContainer.scss'
 
 
 export class CatsContainer extends PureComponent {
@@ -26,10 +27,18 @@ export class CatsContainer extends PureComponent {
       <div className="cats-wrapper">
         <header>
           <Title content="The Amstercats!" />
+          <div className="intro">
+            <p> A prototype website for my (mostly female =) ) friends
+            who adore cats. A user can create and modify profiles for their own
+            cats and share pictures with friends. </p>
+          </div>
         </header>
         <main>
-          { this.props.cats.map(this.renderCat) }
-          < CatsEditor />
+          <div className="cats-profiles">
+            { this.props.cats.map(this.renderCat) }
+          </div>
+            <h2> Add your own Amstercat </h2>
+            < CatsEditor />
         </main>
       </div>
     )
