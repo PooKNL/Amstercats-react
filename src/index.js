a9181087804799a49048352b093c744a6de475d5
Robert Jan Hogerbrugge <rj.hogerbrugge@hotmail.com>
2017-03-19 18:12:14 +0100

    added routes, router, link. history is buggy

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
 import { Provider } from 'react-redux'
-import store from './store'
+import store, { history } from './store'
+import { Router, Route, IndexRoute } from 'react-router'
+
+import App from './App'
+import CatsContainer from './cats/CatsContainer'
+import CatPage from './cats/CatPage'
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
-  </Provider>,  
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={CatsContainer} />
+        <Route path="/cats/:catId" component={CatPage} />
+      </Route>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 )
