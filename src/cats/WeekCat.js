df7d0c7c2d28d694914f5e6e7b53241f780027f3
Robert Jan Hogerbrugge <rj.hogerbrugge@hotmail.com>
2017-05-03 13:53:11 +0200

    part of weekcat functionality

--- src/cats/WeekCat.js
+++ src/cats/WeekCat.js
@@ -5,15 +5,24 @@ export class WeekCat extends PureComponent {
   static propTypes = {}
 
   render() {
+    const { cats } = this.props
+
+    const weekCat = this.props.cats.filter(function (cat) {
+      return cat.likedBy.length === 2
+    })
+
+    const weekName = weekCat.name
+    const weekPhoto = weekCat.photo
+    console.log(weekName)
+
     return(
         <div className="week-cat">
           <h1> Cat of the week! </h1>
           <img id="main-profile-photo" src='http://res.cloudinary.com/daahasvbo/image/upload/v1490779894/Wally4_xyl8zr.jpg' />
-          <h2> Wally </h2>
+          <h2>   </h2>
         </div>
     )
   }
 }
-
 
 export default WeekCat
